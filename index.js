90616cbea62badab45e7e2182d6b35632472e081
h404bi <h404bi@gmail.com>
2017-12-16 12:11:20 +0800

    v0.4.0 Add lazyload

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 /* global hexo */
-'use strict';
+'use strict'
 
-var url = require('url');
-var htmlTag = require('./lib/html_tag');
+var includes = require('array-includes')
+var url = require('url')
+var htmlTag = require('./lib/html_tag')
 
-var rClass = /^[A-Za-z0-9-_]+$/;
-var rSize = /^\d+(?:|\.\d+)(?:%)?$/;
-var rMeta = /["']?([^"']+)?["']?\s*["']?([^"']+)?["']?/;
+var rClass = /^[A-Za-z0-9-_]+$/
+var rSize = /^\d+(?:|\.\d+)(?:%)?$/
+var rMeta = /["']?([^"']+)?["']?\s*["']?([^"']+)?["']?/
 
 /**
 * Figure image tag
@@ -20,51 +21,51 @@ var rMeta = /["']?([^"']+)?["']?\s*["']?([^"']+)?["']?/;
 hexo.extend.tag.register('figure', function(args) {
   function makeUrl(path) {
     if (path[0] === '#' || path.substring(0, 2) === '//') {
-      return path;
+      return path
     }
 
-    var data = url.parse(path);
+    var data = url.parse(path)
 
     if (data && data.protocol) {
-      return path;
+      return path
     }
 
-    path = hexo.config.root + path;
+    path = hexo.config.root + path
 
-    return path.replace(/\/{2,}/g, '/');
+    return path.replace(/\/{2,}/g, '/')
   }
 
   function figureTag(args) {
-    var classes = [];
-    var src, width, height, title, alt;
+    var classes = []
+    var src, width, height, title, alt
 
     // Find all class names
     while (args.length && rClass.test(args[0])) {
-      classes.push(args.shift());
+      classes.push(args.shift())
     }
 
-    classes = classes.join(' ');
+    classes = classes.join(' ')
 
     // Get path of the image
-    src = makeUrl(args.shift());
+    src = makeUrl(args.shift())
 
     if (args.length) {
       // Find image width
       if (rSize.test(args[0])) {
-        width = args.shift();
+        width = args.shift()
         // and height
         if (args.length && rSize.test(args[0])) {
-          height = args.shift();
+          height = args.shift()
         }
       }
 
-      var meta = args.join(' ');
+      var meta = args.join(' ')
 
       // Find image title and alt
       if (meta && rMeta.test(meta)) {
-        var match = meta.match(rMeta);
-        title = match[1];
-        alt = match[2];
+        var match = meta.match(rMeta)
+        title = match[1]
+        alt = match[2]
       }
     }
 
@@ -74,23 +75,32 @@ hexo.extend.tag.register('figure', function(args) {
       height: height,
       title: title,
       alt: alt
-    };
-    var figHtml = '<figure';
+    }
+    var figHtml = '<figure'
 
     if (classes) {
-      figHtml += ' class="' + classes + '"';
+      figHtml += ' class="' + classes + '"'
+      if (includes(classes, 'lazyload')) {
+        imgAttrs = {
+          'data-src': src,
+          width: width,
+          height: height,
+          title: title,
+          alt: alt
+        }
+      }
     }
 
-    figHtml += '>' + htmlTag('img', imgAttrs);
+    figHtml += '>' + htmlTag('img', imgAttrs)
 
     if (title) {
-      figHtml += '<figcaption><p>' + title + '</p></figcaption>';
+      figHtml += '<figcaption><p>' + title + '</p></figcaption>'
     }
 
-    figHtml += '</figure>';
+    figHtml += '</figure>'
 
-    return figHtml;
+    return figHtml
   }
 
-  return figureTag(args);
-});
+  return figureTag(args)
+})
